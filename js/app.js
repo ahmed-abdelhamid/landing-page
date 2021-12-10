c93761c45a59e79c1b93e564b171e9e50a3beeda
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-12-11 02:48:45 +0300

    feat: Add navbar to landing page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+const navbar = document.querySelector("#navbar__list");
+const sections = document.querySelectorAll("section");
+const listItems = document.createDocumentFragment();
 
+sections.forEach((section) => {
+  const listItem = document.createElement("li");
+  const text = section.getAttribute("data-nav");
+  listItem.textContent = text;
+  listItems.appendChild(listItem);
+});
+
+navbar.appendChild(listItems);
+console.log(navbar);
