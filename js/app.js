90df29a9d372d8c34879e9e67621d1babb5a3023
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-12-11 03:04:02 +0300

    feat: Add section links to navbar items

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
-const navbar = document.querySelector("#navbar__list");
-const sections = document.querySelectorAll("section");
-const listItems = document.createDocumentFragment();
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector("#navbar__list");
+  const sections = document.querySelectorAll("section");
+  const listItems = document.createDocumentFragment();
 
-sections.forEach((section) => {
-  const listItem = document.createElement("li");
-  const text = section.getAttribute("data-nav");
-  listItem.textContent = text;
-  listItems.appendChild(listItem);
+  sections.forEach((section) => {
+    const itemId = section.getAttribute("id");
+    const text = section.getAttribute("data-nav");
+
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.setAttribute("href", `#${itemId}`);
+
+    link.textContent = text;
+    listItem.appendChild(link);
+    listItems.appendChild(listItem);
+  });
+
+  navbar.appendChild(listItems);
+  console.log(navbar);
 });
-
-navbar.appendChild(listItems);
-console.log(navbar);
