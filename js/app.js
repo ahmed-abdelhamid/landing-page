4706d078db240aa5b9fd6077a2e8f636628f46f9
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-12-14 07:43:39 +0300

    style: Toggle active class with scrolling

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navbar = document.querySelector("#navbar__list");
   const sections = document.querySelectorAll("section");
-  const listItems = document.createDocumentFragment();
+  const navItems = document.createDocumentFragment();
 
   sections.forEach((section) => {
     const itemId = section.getAttribute("id");
     const text = section.getAttribute("data-nav");
 
-    const listItem = document.createElement("li");
     const link = document.createElement("a");
     link.setAttribute("href", `#${itemId}`);
-
+    link.classList.add("menu__link");
     link.textContent = text;
-    listItem.appendChild(link);
-    listItems.appendChild(listItem);
+
+    const navItem = document.createElement("li");
+    navItem.appendChild(link);
+
+    navItems.appendChild(navItem);
   });
 
-  navbar.appendChild(listItems);
-  console.log(navbar);
+  navbar.appendChild(navItems);
+
+  document.addEventListener("scroll", () => {
+    sections.forEach((section) => {
+      const sectionPosition = section.getBoundingClientRect().top;
+      if (sectionPosition < 300 && sectionPosition > -500) {
+        section.classList.add("active");
+      } else {
+        section.classList.remove("active");
+      }
+    });
+  });
 });
